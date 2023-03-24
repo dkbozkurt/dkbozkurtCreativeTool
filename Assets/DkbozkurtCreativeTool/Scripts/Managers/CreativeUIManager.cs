042c9222659d442d9f56794ca6b72bd61a8c5149
Doğukan Bozkurt <dkaanbozkurt@gmail.com>
2023-03-24 12:24:03 +0300

    UI Manager TODO added

--- Assets/DkbozkurtCreativeTool/Scripts/Managers/CreativeUIManager.cs
+++ Assets/DkbozkurtCreativeTool/Scripts/Managers/CreativeUIManager.cs
@@ -1,9 +1,39 @@
+using System;
 using UnityEngine;
 
 namespace DkbozkurtCreativeTool.Scripts.Managers
 {
     public class CreativeUIManager : MonoBehaviour
     {
+        // TODO this script has to be edited !!!
+        [Header("Hand Images")] 
+        [SerializeField] private GameObject[] _handImages;
+
+        private bool _isMouseFollowerHandActive = false;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                
+            }
+
+        }
+
+        private void MouseFollowerHandSetter()
+        {
+            
+        }
+
+        private void ChangeHandImage()
+        {
+            
+        }
+        
         
     }
 }
