d92e4932bfd3598e98ee7ad7da277e28e0449b05
Doğukan Bozkurt <dkaanbozkurt@gmail.com>
2023-03-27 17:15:01 +0300

    Test script deactivated

--- Assets/CreativeTool/Scripts/Managers/CreativeGameManagerTest.cs
+++ Assets/CreativeTool/Scripts/Managers/CreativeGameManagerTest.cs
@@ -11,43 +11,43 @@ namespace CreativeTool.Scripts.Managers
         //     ScanThroughMethodsWithIEnumerable(iEnumerable);
         // }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.F2))
-            {
-                TestMethod1();
-            }
-        
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                TestMethod2();
-            }
-            
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                TestMethod3();
-            }
-            
-            
-        }
-        
-        [CreativeShortcut(KeyCode.F2,"F2 is the shortcut of TestMethod1! F2 is the shortcut of TestMethod1! F2 is the shortcut of TestMethod1!")]
-        public void TestMethod1()
-        {
-            Debug.Log("F2 Keycode Pressed!");
-        }
-        
-        [CreativeShortcut(KeyCode.W,"W is the shortcut of TestMethod2")]
-        public void TestMethod2()
-        {
-            Debug.Log("W Keycode Pressed!");   
-        }
-        
-        [CreativeShortcut(KeyCode.Space,"Space is the shortcut of TestMethod3")]
-        public void TestMethod3()
-        {
-            Debug.Log("Space Keycode Pressed!");   
-        }
+        // private void Update()
+        // {
+        //     if (Input.GetKeyDown(KeyCode.F2))
+        //     {
+        //         TestMethod1();
+        //     }
+        //
+        //     if (Input.GetKeyDown(KeyCode.W))
+        //     {
+        //         TestMethod2();
+        //     }
+        //     
+        //     if (Input.GetKeyDown(KeyCode.Space))
+        //     {
+        //         TestMethod3();
+        //     }
+        //     
+        //     
+        // }
+        //
+        // [CreativeShortcut(KeyCode.F2,"F2 is the shortcut of TestMethod1! F2 is the shortcut of TestMethod1! F2 is the shortcut of TestMethod1!")]
+        // public void TestMethod1()
+        // {
+        //     Debug.Log("F2 Keycode Pressed!");
+        // }
+        //
+        // [CreativeShortcut(KeyCode.W,"W is the shortcut of TestMethod2")]
+        // public void TestMethod2()
+        // {
+        //     Debug.Log("W Keycode Pressed!");   
+        // }
+        //
+        // [CreativeShortcut(KeyCode.Space,"Space is the shortcut of TestMethod3")]
+        // public void TestMethod3()
+        // {
+        //     Debug.Log("Space Keycode Pressed!");   
+        // }
         
         
         
